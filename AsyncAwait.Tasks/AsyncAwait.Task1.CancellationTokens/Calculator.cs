66d05cd73ed750e41471a7a5a4c302eadff12e15
Roman Nehaikov <Roman_Nehaikov@epam.com>
2023-04-17 19:40:20 +0300

    First Task completed

---
+++ AsyncAwait.Tasks/AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait.Task1.CancellationTokens;
+
+internal static class Calculator
+{
+    // todo: change this method to support cancellation token
+    public static Task<long> Calculate(int n, CancellationToken token)
+    {
+        return Task.Factory.StartNew(() =>
+        {
+            long sum = 0;
+
+            for (var i = 0; i < n; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    return 0;
+                }
+
+                // i + 1 is to allow 2147483647 (Max(Int32)) 
+                sum += (i + 1);
+                Thread.Sleep(10);
+            }
+
+            return sum;
+        }, token);
+    }
+}
