a33c4b8c4024b2d3e1a504a2b03411649c16fa2c
Roman Nehaikov <Roman_Nehaikov@epam.com>
2023-05-05 19:50:58 +0300

    Complete first task and load sources for second

---
+++ Analyzing and profiling tools/FirstTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Analyzing_and_profiling_tools
+{
+    public class FirstTask
+    {
+        public static string GeneratePasswordHashUsingSaltOldImplementation(string passwordText, byte[] salt)
+        {
+            var iterate = 10000;
+            var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+            var hash = pbkdf2.GetBytes(20);
+            var hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+            var passwordHash = Convert.ToBase64String(hashBytes);
+            return passwordHash;
+        }
+
+        public static string GeneratePasswordHashUsingSaltNewImplementation(string passwordText, byte[] salt)
+        {
+            const int iterate = 10000;
+            const int bytesCount = 20;
+            const int magicNumber = 16;
+            var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+            var hash = pbkdf2.GetBytes(bytesCount);
+            var hashBytes = new byte[magicNumber + bytesCount];
+
+            Buffer.BlockCopy(salt, 0, hashBytes, 0, magicNumber);
+            Buffer.BlockCopy(hash, 0, hashBytes, magicNumber, bytesCount);
+
+            return Convert.ToBase64String(hashBytes);
+        }
+
+    }
+}
---
+++ Analyzing and profiling tools/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Analyzing_and_profiling_tools
+{
+    public class Program
+    {
+        private const string Password = "P@SSWORD";
+        static void Main(string[] args)
+        {
+            var salt = Encoding.UTF8.GetBytes("SoMeSaLtVaLuE1234567890");
+            OldPerformance(salt);//410382
+            NewPerformance(salt);//396220
+        }
+
+        public static void OldPerformance(byte[] salt)
+        {
+            var watcher = new Stopwatch();
+            watcher.Start();
+            FirstTask.GeneratePasswordHashUsingSaltOldImplementation(Password, salt);
+            watcher.Stop();
+            Console.WriteLine(watcher.ElapsedTicks);
+        }
+
+        public static void NewPerformance(byte[] salt)
+        {
+            var watcher = new Stopwatch();
+            watcher.Start();
+            FirstTask.GeneratePasswordHashUsingSaltNewImplementation(Password, salt);
+            watcher.Stop();
+            Console.WriteLine(watcher.ElapsedTicks);
+        }
+
+    }
+}
